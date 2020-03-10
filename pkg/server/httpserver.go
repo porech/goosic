ee4f50eaf58e0e0ab315d38d972ce385e08aaa6d
xela92 <xelasreborn@gmail.com>
2020-03-10 23:25:37 +0100

    embedded react gui into goosic server

--- pkg/server/httpserver.go
+++ pkg/server/httpserver.go
@@ -2,14 +2,34 @@ package server
 
 import (
 	"fmt"
+	"net/http"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/gobuffalo/packr/v2"
 	"github.com/porech/goosic/v2/pkg/storage"
 	log "github.com/sirupsen/logrus"
 )
 
 type HttpServer struct {
 	Store *storage.Storage
+}
+
+func PackrMiddleware(urlPrefix string, box *packr.Box) gin.HandlerFunc {
+	fs := http.FileServer(box)
+	return func(c *gin.Context) {
+		if c.Request.URL.Path == "/" {
+			index, err := box.Find("index.html")
+			if err != nil {
+				fs.ServeHTTP(c.Writer, c.Request)
+				return
+			}
+			c.Writer.WriteHeader(200)
+			c.Writer.Write(index)
+			return
+		}
+		fs.ServeHTTP(c.Writer, c.Request)
+	}
 }
 
 func (s *HttpServer) StartServer(host string, port int, corsOrigin string) error {
@@ -21,6 +41,10 @@ func (s *HttpServer) StartServer(host string, port int, corsOrigin string) error
 		r.Use(cors.New(config))
 	}
 
+	// Create the box for the frontend page
+	box := packr.New("GreyGoosic", "../../greygoosic/build")
+
+	r.Use(PackrMiddleware("/", box))
 	r.GET("/song-list", s.songList)
 	r.GET("/song/:id", s.song)
 	r.GET("/song-stream/:id", s.streamSong)
