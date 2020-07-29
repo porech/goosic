9ce1adce8d32ca604a3ea0f5a3fed3dcfdd9158a
xela92 <xelasreborn@gmail.com>
2020-07-30 00:10:08 +0200

    work in progress: queue-next with view approach

--- greygoosic/src/actions/index.js
+++ greygoosic/src/actions/index.js
@@ -1,26 +1,12 @@
 import { SEARCH_SONG } from "../constants";
 import { PLAYER_PAUSE, PLAYER_RESUME, PLAYER_SEEK } from "../state/player";
-import {
-  QUEUE_ADD_SONG,
-  QUEUE_NEXT_SONG,
-  QUEUE_PREVIOUS_SONG
-} from "../state/queue-next"
-
-export const addSongToQueue = (song) => {
-  return {
-    type: QUEUE_ADD_SONG,
-    payload: {
-      song
-    }
-
-  }
-}
+import { QUEUE_NEXT_SONG, SONG_FROM_VIEW } from "../state/queue-next";
 export const nextSong = () => {
   return { type: QUEUE_NEXT_SONG };
 };
 
 export const previousSong = () => {
-  return { type: QUEUE_PREVIOUS_SONG };
+  return { type: "QUEUE_PREVIOUS_SONG" };
 };
 
 export const pauseSong = () => {
@@ -40,12 +26,11 @@ export const seekTo = (position) => ({
   payload: position,
 });
 
-export const enqueueSongs = (songs, index, playNow) => ({
-  type: QUEUE_ADD_SONG,
+export const songFromView = (view, viewIndex) => ({
+  type: SONG_FROM_VIEW,
   payload: {
-    songs,
-    index,
-    playNow,
+    view,
+    viewIndex,
   },
 });
 
--- greygoosic/src/state/queue-next.js
+++ greygoosic/src/state/queue-next.js
@@ -1,198 +1,108 @@
-import { put, select, takeEvery } from "redux-saga/effects"
+import { put, select, takeEvery } from "redux-saga/effects";
 import { shuffle } from "lodash";
-import {PLAYER_PLAY_SONG} from './player';
-import {getSongs} from './songs';
+import { PLAYER_PLAY_SONG, PLAYER_ENDED } from "./player";
+import { getSongs } from "./songs";
 import { NEXT_SONG } from "../constants";
-export const SHUFFLE = {
-    NONE:-1,
-    NORMAL: 0,
-    ALL: 1
-}
+
 export const REPEAT = {
-    NONE: -1,
-    NORMAL: 0,
-    ALL: 1
-}
-export const ADD_QUEUE = "ADD_QUEUE"
-export const REMOVE_FIRST_QUEUE = "REMOVE_FIRST_QUEUE"
-export const QUEUE_NEXT_SONG = "QUEUE_NEXT_SONG"
-export const QUEUE_PREVIOUS_SONG = "QUEUE_PREVIOUS_SONG"
-export const PLAY_LATEST_QUEUE = "PLAY_LATEST_QUEUE"
-export const QUEUE_ADD_SONG = "QUEUE_ADD_SONG"
-export const NEXT_QUEUE = "NEXT_QUEUE"
-export const UPDATE_QUEUE_OFFSET = "UPDATE_QUEUE_OFFSET"
-export const UPDATE_QUEUE_SKIP = "UPDATE_QUEUE_SKIP"
+  NONE: -1,
+  ONE: 0,
+  ALL: 1,
+};
+export const ADD_QUEUE = "ADD_QUEUE";
+export const REMOVE_FIRST_QUEUE = "REMOVE_FIRST_QUEUE";
+export const QUEUE_NEXT_SONG = "QUEUE_NEXT_SONG";
+export const QUEUE_PREVIOUS_SONG = "QUEUE_PREVIOUS_SONG";
+export const PLAY_LATEST_QUEUE = "PLAY_LATEST_QUEUE";
+export const SONG_FROM_VIEW = "SONG_FROM_VIEW";
+const NEXT_SONG_INDEX = "NEXT_SONG_INDEX";
+const NEXT_QUEUE_INDEX = "NEXT_QUEUE_INDEX";
+const QUEUE_PLAY_SONG = "QUEUE_PLAY_SONG";
 
-//export const REMOVE_QUEUE = "REMOVE_QUEUE"
-const getQueuedSong = state => {
-    const { queues, queuesShuffled, currentQueueIndex, currentIndex, shuffle} = state.queue;
-    //if shuffle is enabled, use the shuffled queue, else use the normal queue
-    return shuffle !== SHUFFLE.NONE ? queuesShuffled[currentQueueIndex][currentIndex] : queues[currentQueueIndex][currentIndex]
-  }
-
-const getQueueSkip = state => {
-    return state.queue.skip;
-}
-const getQueueOffset = state => {
-    return state.queue.offset;
-}
+const getQueuedSong = (state) => {
+  const { queues, queueIndex, songIndex } = state.queue;
+  return queues[queueIndex][songIndex];
+};
 
 //#TODO should return songs based on the context we're on; for instance, if the context is an artist view, should return songs made by this artist
-const getContextSongs = state => {
-    return state.songs.songs;
-}
+const getContextSongs = (state) => {
+  return state.songs.songs;
+};
 
 let defaultState = {
-    queues: [[]],
-    queuesShuffled: [],
-    currentIndex: -1,
-    currentQueueIndex: 0,
-    shuffle: SHUFFLE.NONE,
-    repeat: REPEAT.NONE,
-    offset: 0,
-    skip: 0
-}
+  //array of queue arrays
+  queues: [[]],
+  //current play index in the queue
+  songIndex: -1,
+  //current queue index
+  queueIndex: -1,
+  //current song view, from which we populate the queue
+  view: [],
+  viewIndex: -1,
+  shuffle: false,
+  repeat: REPEAT.NONE,
+};
 export const reducer = (state = defaultState, action) => {
-    let { currentIndex, currentQueueIndex, queues, repeat} = state;
-    switch(action.type) {
-        case ADD_QUEUE:
-            if (queues.length === 5) put({type: REMOVE_FIRST_QUEUE})
-                queues.push(action.payload)
-                //console.log("new queue pushed", queues[queues.length-1])
-            return {
-                ...state,
-                queues: queues
-            }
-        case QUEUE_ADD_SONG:
-            queues[currentQueueIndex].push(action.payload.song)
-            //setting currentIndex to length because it will be next index (to go next)
-            return {
-                ...state,
-                queues,
-                currentIndex: queues[currentQueueIndex].length - 1
-            }
-        case REMOVE_FIRST_QUEUE:
-            return {
-                ...state,
-                queues: queues.splice(0)
-            }
-        case NEXT_QUEUE:
-            return {
-                ...state,
-                currentQueueIndex: currentQueueIndex+1,
-                currentIndex: 0
-            }
-        case QUEUE_NEXT_SONG:
-            //check if we're on the last track in the queue, and there's another queue to switch to
-            if (currentIndex === queues[currentQueueIndex].length - 1 && queues[currentQueueIndex+1]) {
-                console.log("switching to next queue")
-                switch (repeat) {
-                    case REPEAT.NONE || REPEAT.ALL:
-                        //if repeat is not set, we try to switch to the next queue, first song if we're not in the last one
-                        if (currentQueueIndex < queues[currentQueueIndex].length - 1) return { ...state, currentQueueIndex: currentQueueIndex+1, currentIndex: 0}
-                        //if we're in the last queue and repeat is not set, just do nothing
-                        if (currentQueueIndex === queues[currentQueueIndex].length - 1 && repeat === REPEAT.NONE) return { ...state}
-                        //if we're in the last queue and repeat is set to ALL, we start again from first queue, first song
-                        if (currentQueueIndex === queues[currentQueueIndex].length - 1 && repeat === REPEAT.ALL) return {...state, currentQueueIndex: 0, currentIndex: 0}
-                        break;
-                    case REPEAT.NORMAL:
-                        //if we're repeating in normal mode, we should just repeat the current queue
-                        return {...state, currentIndex: 0}
+  const { queueIndex, queues, songIndex, view, viewIndex } = state;
 
-                }
-            }
-            return {
-                ...state,
-                currentIndex: currentIndex+1
-            }
-        case PLAY_LATEST_QUEUE:
-            return {
-                ...state,
-                currentQueueIndex: queues.length - 1,
-                currentIndex: 0
-            }
-        case QUEUE_PREVIOUS_SONG:
-            //check if we're on the first track in the queue and there's another queue to switch to
-            if (currentIndex === 0 && queues[currentQueueIndex-1]) {
-                switch (repeat) {
-                    case REPEAT.NONE || REPEAT.ALL:
-                        //if repeat is not set, we try to switch to the previous queue, last song if we're not in the first one
-                        if (currentQueueIndex > 0) return { ...state, currentQueueIndex: currentQueueIndex-1, currentIndex: queues[currentQueueIndex-1].length - 1}
-                        //if we're in the first queue and repeat is not set, just do nothing
-                        if (currentQueueIndex === 0 && repeat === REPEAT.NONE) return { ...state}
-                        //if we're in the first queue and repeat is set to ALL, we start again from last queue, last song
-                        if (currentQueueIndex === 0 && repeat === REPEAT.ALL) return {...state, currentQueueIndex: queues.length - 1, currentIndex: queues[queues.length-1].length - 1}
-                        break;    
-                    case REPEAT.NORMAL:
-                        //if we're repeating in normal mode, we should just repeat the current queue
-                        return {...state, currentIndex: queues[currentQueueIndex].length - 1}
-                }
-            }
-            return {
-                ...state,
-                currentIndex: currentIndex-1
-            }
-        case UPDATE_QUEUE_SKIP:
-            return {...state, skip: action.payload}
-        case UPDATE_QUEUE_OFFSET:
-            return {...state, offset: action.payload}
-        default:
-            return state;
+  switch (action.type) {
+    case SONG_FROM_VIEW:
+      //index in the view
+      const queue = [view[viewIndex]];
+      if (queueIndex === queues.length - 1) {
+        queues.push(queue);
+      } else {
+        queues.splice(queueIndex, 0, queue);
+      }
+      //updating songIndex to 0 to be ready to play the next song
+      return {
+        ...state,
+        view,
+        queues,
+        songIndex: 0,
+        queueIndex: queueIndex + 1,
+        viewIndex,
+      };
+    case NEXT_SONG_INDEX:
+      return {
+        ...state,
+        songIndex: songIndex + 1,
+      };
+    case NEXT_QUEUE_INDEX:
+      return {
+        ...state,
+        queueIndex: queueIndex + 1,
+        songIndex: 0,
+      };
 
-    }
-
-}
-
-function* addQueue(action) {
-    if (action.playNow) {
-        yield put({type: PLAY_LATEST_QUEUE})
-    }
+    default:
+      return state;
+  }
+};
+function* nextSong() {
+  //evaluates queue status and gets new songs if needed
+  const { queues, queueIndex, songIndex } = yield select(
+    (state) => state.queue
+  );
+  const currentQueue = queues[queueIndex];
+  //if there are songs left in the queue
+  if (currentQueue.length > songIndex + 1) {
+    yield put({ type: NEXT_SONG_INDEX });
+    yield put({ type: QUEUE_PLAY_SONG });
+    return;
+  }
+  if (queues.length > queueIndex + 1) {
+    yield put({ type: NEXT_QUEUE_INDEX });
+    yield put({ type: QUEUE_PLAY_SONG });
+    return;
+  }
 }
 function* playSong() {
-    const song = yield select(getQueuedSong)
-    //#TODO better naming
-    let skip = yield select(getQueueSkip)
-    let offset = yield select(getQueueOffset)
-    const allSongs = yield select(getSongs)
-    //if I don't have a queued song
-    if (song === undefined) {
-        //get some songs *related to the queue* and add them to queue
-        let songs = yield select(getContextSongs)
-        //#TODO implement skip / offset in a meaningful way
-/*         if (offset === 0) {
-            offset = songs.length / 2;
-        } */
-        yield put({
-            type: ADD_QUEUE,
-            //payload: songs.slice(skip,offset),
-            payload: songs.slice(0, songs.length),
-            playNow: true
-        })
-/*         if (skip === 0) {
-            yield put({
-                type: UPDATE_QUEUE_SKIP,
-                payload: offset
-            })
-        } */
-        yield put({
-            type: UPDATE_QUEUE_OFFSET,
-            payload: offset+skip
-
-        })
-    } else {
-        console.log("song", song)
-        yield put({
-            type: PLAYER_PLAY_SONG,
-            payload: song
-        })
-    }
+  const song = yield select(getQueuedSong);
+  yield put({ type: PLAYER_PLAY_SONG, payload: song });
 }
 
 export function* saga() {
-    yield takeEvery("ADD_QUEUE", addQueue)
-    yield takeEvery("QUEUE_NEXT_SONG", playSong)
-    yield takeEvery("QUEUE_PREVIOUS_SONG", playSong)
-    yield takeEvery("QUEUE_ADD_SONG", playSong)
-    yield takeEvery("PLAY_LATEST_QUEUE", playSong)
-
+  yield takeEvery([SONG_FROM_VIEW, QUEUE_PLAY_SONG], playSong);
+  yield takeEvery([QUEUE_NEXT_SONG, PLAYER_ENDED], nextSong);
 }
