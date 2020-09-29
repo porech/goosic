7813acdf2db96fef20ab661b71d712ea1a0e106f
xela92 <xelasreborn@gmail.com>
2020-09-29 17:53:04 +0200

    fix proxy, now correctly reads PROXY env variable and creates proxy for /api route

--- greygoosic/src/setupProxy.js
+++ greygoosic/src/setupProxy.js
@@ -1,11 +1,9 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const pkg = require("../package.json");
 const target = process.env.PROXY || pkg.proxy;
-
 module.exports = (app) =>
-  target &&
-  app.use((req, res, next) =>
-    req.accepts("text/html")
-      ? next()
-      : createProxyMiddleware({ target: target })(req, res, next)
+  app.use("/api",
+      createProxyMiddleware({ target: target,
+        changeOrigin: true
+      })
   );
