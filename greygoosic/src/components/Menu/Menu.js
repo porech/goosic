5ded030523d47df2c0121672573139cd653c6b2c
xela92 <xelasreborn@gmail.com>
2020-10-03 15:17:21 +0200

    much better menu

--- greygoosic/src/components/Menu/Menu.js
+++ greygoosic/src/components/Menu/Menu.js
@@ -6,28 +6,27 @@ const ReactRouter = require("react-router");
 
 const Menu = () => {
     const browserHistory = ReactRouter.useHistory();
-    var dimension = "massive";
     return (
         <div className="menu-outer-container">
             <div className="menu-back-button menu-overlay">
               <i onClick={() => {browserHistory.goBack()}} className="arrow circle left icon huge violet"></i>
             </div>
                 <Media queries={{
-                        small: "(max-width: 900px)",
-                        large: "(min-width: 1200px)"
+                        small: "(max-width: 800px)",
+                        large: "(min-width: 800px)"
                 }}>
                   {matches => (
                     <div className="menu-container">
                         <Link className="menu-item" to="/artist">
-                                <i className={`user circle left icon violet ${matches.small ? 'small' : 'massive'}`}></i><br></br><div className="menu-item-label">Artists</div>
+                                <i className={`menu-item-icon user circle left icon violet ${matches.small ? 'large' : 'huge'}`}></i><br></br><div className="menu-item-label">Artists</div>
                         </Link>
                         
                         <Link className="menu-item" to="/album">
-                                <i className={`menu-item-icon bullseye left icon violet ${matches.small ? 'small' : 'massive'}`}></i><br></br><div className="menu-item-label">Albums</div>
+                                <i className={`menu-item-icon bullseye left icon violet ${matches.small ? 'large' : 'huge'}`}></i><br></br><div className="menu-item-label">Albums</div>
                         </Link>                               
                         
                         <Link className="menu-item" to="/">
-                                <i className={`menu-item-icon music circle left icon violet ${matches.small ? 'small' : 'massive'}`}></i><br></br><div className="menu-item-label">Songs</div>
+                                <i className={`menu-item-icon music circle left icon violet ${matches.small ? 'large' : 'huge'}`}></i><br></br><div className="menu-item-label">Songs</div>
                         </Link>
                     </div>
                   )}
